2e973a3394f1abd50e59d8820e957cad296a561c
webninjataylor <webninjataylor@gmail.com>
2015-04-08 20:53:05 -0400

    Added comments for what needs to be done for HW-1.

---
+++ bookmark.rb
@@ -0,0 +1,6 @@
+# TO DO ...
+# Create a class named Bookmark with the following:
+#   An initializer that accepts url and title parameters:
+#     it should set instance variables named url, title
+#     it should set a read only variable named last_visited
+#   A method named visit! which sets last_visited to the current time and date (hint: the Time.now method returns an instance of Time)
---
+++ functions.rb
@@ -0,0 +1,37 @@
+# Return a reversed copy of the array
+def reverse(an_array)
+  # TODO write your code here
+end
+
+# Return a map of letters and counts of letters
+# Letters should be lowercase before counting
+# For example, histogram('Hello') returns {'h'=>1,'e'=>1,'l'=>2,'o'=>1}
+def histogram(a_string)
+  # TODO write your code here
+end
+
+# Sum all the numbers in the array
+# an_array can contain lots of different things
+# Be sure to only sum the numbers
+# (Hint: the is_a? method might be useful here)
+def sum_only_numbers(an_array)
+  # TODO write your code here
+end
+
+# For i from 1 to 100, return an array.
+# The elements in the array should follow this algorithm:
+# If i is a multiple of 3, the element is 'Fizz'
+# If i is a multiple of 5, the element is 'Buzz'
+# If i is a multiple of 3 and 5, the element is 'FizzBuzz'
+# Otherwise, the element is simply the value of i
+# For example [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', ..., 14, 'FizzBuzz', ...]
+def fizzbuzz
+  # TODO write your code here
+end
+
+# Uncomment a line to test the function you are developing
+# Type "ruby functions.rb" at command line (Konsole) to run the script
+# puts reverse([3,6,'dog']).inspect
+# puts histogram('The Quick brown fox').inspect
+# puts sum_only_numbers [4, 'foo', [ ], 27, :rain, 3.14]
+# puts fizzbuzz.join("\n")
